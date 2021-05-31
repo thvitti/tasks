a4e0927925f51106b781dc853f042fd84569056a
thvitti <thvittix@gmail.com>
2021-05-31 15:30:26 -0300

    remove all tasks operation added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :tasks do
+    collection do
+      delete :destroy_all
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
