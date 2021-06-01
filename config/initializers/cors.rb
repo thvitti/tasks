b52671ace2d7697fbc80fb6bb707332e7402bcd4
thvitti <thvittix@gmail.com>
2021-06-01 10:19:13 -0300

    cors setup

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'https://rails-react-task.herokuapp.com/'
+     origins 'https://rails-react-task.herokuapp.com'
 
      resource '*',
        headers: :any,
