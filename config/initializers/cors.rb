2099c1cf2fd6c2ad9be1db07a1c7ade3a29950b9
thvitti <thvittix@gmail.com>
2021-05-31 21:20:22 -0300

    cors setup changed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins '*'
+     origins 'https://rails-react-task.herokuapp.com/'
 
      resource '*',
        headers: :any,
